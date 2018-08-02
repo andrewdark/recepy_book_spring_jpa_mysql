ba8d8bc8dd58cab5baef4baa14585a469dc07e39
andrewdark <samsonov.a@ukr.net>
2018-08-02 17:21:48 +0300

    Add UnionOfMeasureConverter

--- src/main/java/ua/pp/darknsoft/converters/UnitOfMeasureCommandToUnitOfMeasure.java
+++ src/main/java/ua/pp/darknsoft/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -1,4 +1,20 @@
 package ua.pp.darknsoft.converters;
 
-public class UnitOfMeasureCommandToUnitOfMeasure {
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import ua.pp.darknsoft.commands.UnitOfMeasureCommand;
+import ua.pp.darknsoft.models.UnitOfMeasure;
+
+@Component
+public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure> {
+    @Nullable
+    @Override
+    public UnitOfMeasure convert(UnitOfMeasureCommand source) {
+        if (source == null) return null;
+        final UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(source.getId());
+        uom.setDescription(source.getDescription());
+        return uom;
+    }
 }
--- src/main/java/ua/pp/darknsoft/converters/UnitOfMeasureToUnitOfMeasureCommand.java
+++ src/main/java/ua/pp/darknsoft/converters/UnitOfMeasureToUnitOfMeasureCommand.java
@@ -1,4 +1,16 @@
 package ua.pp.darknsoft.converters;
 
-public class UnitOfMeasureToUnitOfMeasureCommand {
+import org.springframework.core.convert.converter.Converter;
+import ua.pp.darknsoft.commands.UnitOfMeasureCommand;
+import ua.pp.darknsoft.models.UnitOfMeasure;
+
+public class UnitOfMeasureToUnitOfMeasureCommand implements Converter<UnitOfMeasure,UnitOfMeasureCommand> {
+    @Override
+    public UnitOfMeasureCommand convert(UnitOfMeasure source) {
+        if (source == null) return null;
+        final UnitOfMeasureCommand uomc = new UnitOfMeasureCommand();
+        uomc.setId(source.getId());
+        uomc.setDescription(source.getDescription());
+        return uomc;
+    }
 }
